8ca3cbdf8b8e3c36ac090997edb71d53f065f835
Phillipe Murray <phi.coimbra@gmail.com>
2024-10-30 20:07:23 -0300

    chore: generate build

---
+++ src/app/api/scraper/route.ts
@@ -0,0 +1,30 @@
+import chromium from "@sparticuz/chromium";
+import puppeteer from "puppeteer-core";
+
+chromium.setHeadlessMode = true;
+chromium.setGraphicsMode = false;
+
+export async function POST() {
+  await chromium.font(
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
+
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://spacejelly.dev");
+  const pageTitle = await page.title();
+  await browser.close();
+
+  return Response.json({
+    pageTitle,
+  });
+}
