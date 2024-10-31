fae2ace58980a1410df966c5eb463e7833476064
Phillipe Murray <phi.coimbra@gmail.com>
2024-10-31 10:48:08 -0300

    chore: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer",
+      "puppeteer-core",
+      "@sparticuz/chromium",
+    ],
+  },
+};
 
 export default nextConfig;
