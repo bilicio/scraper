f8a6ffc13e13053980dcfee775123dc120c53985
Phillipe Murray <phi.coimbra@gmail.com>
2024-10-31 10:12:35 -0300

    chore: rota public

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -14,7 +14,7 @@ export async function POST() {
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath("../"),
+    executablePath: await chromium.executablePath("/app/public/"),
     headless: chromium.headless,
   });
 
